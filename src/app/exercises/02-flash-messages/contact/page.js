07ef25389f180c6588dc777b67709358bd2bf8d9
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-12-01 09:33:46 +0200

    Finish exercise 2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,15 +1,23 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 function ContactPage() {
+  const nextRouter = useRouter()
+
   return (
     <main>
-      <form>
+      <form
+        onSubmit={function (event) {
+          event.preventDefault();
+          nextRouter.push('/exercises/02-flash-messages')
+        }}
+      >
         <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+        <input id="name" required={true} value={'name'}/>
 
         <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+        <textarea id="message" value={'message'}/>
 
         <button>Submit</button>
       </form>
--- src/app/exercises/02-flash-messages/page.js
+++ src/app/exercises/02-flash-messages/page.js
@@ -1,7 +1,12 @@
+'use client'
 import React from 'react';
 import Link from 'next/link';
+import Toast from '../../../components/Toast';
+import { ToastContext } from '../../../components/ToastProvider';
 
 function FlashMsgExercise() {
+  const {createToast} = React.useContext(ToastContext)
+  React.useEffect(() => createToast("Message Sent", "success"), [])
   return (
     <main>
       <h1>Welcome to my website!</h1>
